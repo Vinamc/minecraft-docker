e62d81f726a5b9f8dded20b58dcefe98ed110c87
Alex <41188285+alexzvn@users.noreply.github.com>
2021-09-30 11:23:08 +0700

    add automation script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for VERSION in alpha beta delta
+do
+    docker build -t spigot-runner:${VERSION} -f ${VERSION}.dockerfile .
+    docker image tag spigot-runner:${VERSION} alexzvn/spigot-runner:${VERSION}
+    docker push alexzvn/spigot-runner:${VERSION}
+done
